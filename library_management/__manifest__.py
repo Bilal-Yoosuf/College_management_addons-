b8b823ddcb39f51247ee44218fb0c13389cbee73
Bilal-Yoosuf <bilalyoosuf22@gmail.com>
2021-01-04 10:05:12 +0530

    added college

--- library_management/__manifest__.py
+++ library_management/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Library Management",
     'version': '13.1',
-    'depends': ['base',],
+    'depends': ['base', 'website'],
     'author': "Socius Trainee",
     # 'category': 'Category',
     # 'description': "Library management module",
@@ -14,8 +14,9 @@
         'views/book_category.xml',
         'views/res_partner_inherit_form.xml',
         'views/book_shelf.xml',
+        'views/front_end/webpages/first_page.xml',
         'reports/report.xml',
-        'reports/book_report.xml'
+        'reports/book_report.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
--- library_management/models/book_registration.py
+++ library_management/models/book_registration.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class BookRegistration(models.Model):
@@ -15,6 +15,46 @@ class BookRegistration(models.Model):
     category_id = fields.Many2one('book.category', string="Category")
     published_year = fields.Integer(string="Published Year")
     shelf_id = fields.Many2one('book.shelf', string="Book Shelf")
+    student_ids = fields.One2many('book.status', 'book_id', string="Book orders", compute='_automate_books_list')
+
+    @api.depends('name')
+    def _automate_books_list(self):
+        print("inside depends")
+        for record in self:
+            # self.name = "Book1"
+            student_rec = self.env['book.return'].search([('name', '=', self.id)])
+            print("student records", student_rec)
+            for student in student_rec:
+                line = {
+                    'book_id': self.id,
+                    'student_name': student.sname.name,
+                    'student_taken_type': student.taken_type,
+                    'book_title': student.name.name
+                }
+                print("line", line)
+                record.student_ids.create(line)
+
+
+# @api.model
+# def default_get(self, fields):
+#     res = super(BookRegistration, self).default_get(fields)
+#     student_lines = [(5, 0, 0)]
+#     student_rec = self.env['book.return'].search([])
+#     # print(student_rec)
+#     for student in student_rec:
+#             print(self.name)
+#             # print(student.sname.name)
+#             line = (0, 0, {
+#                 'student_name': student.sname.name,
+#                 'student_taken_type': student.taken_type,
+#                 'book_title': student.name.name
+#             })
+#             student_lines.append(line)
+#     res.update({
+#         'student_ids': student_lines,
+#
+#     })
+#     return res
 
 
 class BookCategory(models.Model):
@@ -31,4 +71,12 @@ class BookAuthor(models.Model):
 class BookShelf(models.Model):
     _name = "book.shelf"
     name = fields.Char(string="Shelf ID")
-    book_ids = fields.One2many('book.registration', 'shelf_id',  string="Book")
+    book_ids = fields.One2many('book.registration', 'shelf_id', string="Book")
+
+
+class BookStatus(models.Model):
+    _name = 'book.status'
+    student_name = fields.Char(string='Student')
+    book_title = fields.Char(string='Book')
+    student_taken_type = fields.Char(string="Taken Type")
+    book_id = fields.Many2one('book.registration', string="Book Name")
